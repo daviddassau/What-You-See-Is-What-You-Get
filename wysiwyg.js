96ab4c578d6b17b00c18863eb6b2518d5b095d0d
David Dassau <todieahero@hotmail.com>
2017-08-23 11:36:18 -0500

    styled person elements

--- wysiwyg.js
+++ wysiwyg.js
@@ -23,7 +23,7 @@ var famousPeopleArray = [
 	{
 	title: "Musician",
 	name: "Andrew McMahon",
-	image: "https://images.genius.com/d651b980e15f0404dad7e5f46edb43c6.628x378x1.jpg",
+	image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Andrew_McMahon_playing_in_2014.jpg/220px-Andrew_McMahon_playing_in_2014.jpg",
 	lifespan: {
 		birth: 1982,
 		death: "TBD"
@@ -32,7 +32,7 @@ var famousPeopleArray = [
 	{
 	title: "Comedian",
 	name: "Jim Jefferies",
-	image: "http://thelaughbutton.com/site2/wp-content/uploads/2017/03/jeffries.jpg",
+	image: "https://pixel.nymag.com/imgs/daily/vulture/2017/06/05/05-jim-jefferies-chatroom-silo.w245.h368.2x.png",
 	lifespan: {
 		birth: 1970,
 		death: "TBD"
@@ -47,9 +47,13 @@ for (var i = 0; i < famousPeopleArray.length; i++){
 	var personString = famousPeopleArray[i];
 	var domString = "";
 
-	domString += `<header>${personString.name}, ${personString.title}</header>`;
-	domString += `<section><img src="${personString.image}" width="200"></section>`;
-	domString += `<footer>${famousPeopleArray[i].lifespan.birth}, ${famousPeopleArray[i].lifespan.death}</footer>`;
+	// domString += `<header>${personString.name}, ${personString.title}</header>`;
+	domString += `<div id="personContainerDiv">`;
+	domString +=   `<h2 class="lightBlueBG">${personString.name}</h2>`;
+	domString +=   `<h4 class="yellowBG">${personString.title}</h4>`;
+	domString +=   `<section class="lightBlueBG" id="famousPersonImage"><img src="${personString.image}"></section>`;
+	domString +=   `<footer class="yellowBG">${famousPeopleArray[i].lifespan.birth}, ${famousPeopleArray[i].lifespan.death}</footer>`;
+	domString += `</div>`;
 
 
 	personContainer.innerHTML += domString;
